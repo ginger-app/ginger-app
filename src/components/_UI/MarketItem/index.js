e18b2834da8ca46f1a6b8941f35478dadfd56f3e
roquefore <roquefore@gmail.com>
2020-04-19 18:12:38 +0300

    [UPD] MarketItem -> Transition

--- src/components/_UI/MarketItem/index.js
+++ src/components/_UI/MarketItem/index.js
@@ -2,12 +2,14 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { Transition } from 'react-transition-group';
 
 // Styles
 import Styles from './styles.module.scss';
 
 // Instruments
 import { Icon, MarketItemOverlay } from 'components';
+import { opacityTransitionConfig } from 'utils/transitionConfig';
 import heart from 'theme/assets/svg/heart.svg';
 import heartFilled from 'theme/assets/svg/heart-filled.svg';
 import apples from 'theme/assets/images/apples-mock.png';
@@ -20,33 +22,55 @@ const mapDispatchToProps = {};
 
 const MarketItemComponent = ({ className, style, to, name }) => {
     // State
-    const [overlayEnabled, setOverlayState] = useState(true);
+    const [overlayEnabled, setOverlayState] = useState(false);
     const [heartHovered, setHeartHoveredState] = useState(false);
 
     return overlayEnabled ? (
         <MarketItemOverlay setOverlayState={setOverlayState} style={style} className={className} />
     ) : (
-        <NavLink className={`${Styles.container} ${className}`} style={style} to={to}>
-            <img
-                src={heartHovered ? heartFilled : heart}
-                className={Styles.heart}
-                onMouseEnter={() => setHeartHoveredState(true)}
-                onMouseLeave={() => setHeartHoveredState(false)}
-                alt=''
-            />
-            <img src={apples} alt='' className={Styles.itemImage} />
-            <p className={Styles.itemName}>{name}</p>
-            <p className={Styles.price}>
-                46<span>.99$</span>
-            </p>
-            <p className={Styles.amount}>1 kg</p>
-            <Icon
-                name='cart'
-                color='white'
-                className={Styles.cart}
-                onClick={() => setOverlayState(true)}
-            />
-        </NavLink>
+        <Transition
+            in
+            appear
+            mountOnEnter
+            unmountOnExit
+            timeout={opacityTransitionConfig().timeout}
+        >
+            {(state) => (
+                <NavLink
+                    className={`${Styles.container} ${className}`}
+                    to={to}
+                    style={{
+                        ...style,
+                        ...opacityTransitionConfig().defaultStyles,
+                        ...opacityTransitionConfig().transitionStyles[state],
+                    }}
+                >
+                    <img
+                        src={heartHovered ? heartFilled : heart}
+                        className={Styles.heart}
+                        onMouseEnter={() => setHeartHoveredState(true)}
+                        onMouseLeave={() => setHeartHoveredState(false)}
+                        alt=''
+                    />
+                    <img src={apples} alt='' className={Styles.itemImage} />
+                    <p className={Styles.itemName}>{name}</p>
+                    <p className={Styles.price}>
+                        46<span>.99$</span>
+                    </p>
+                    <p className={Styles.amount}>1 kg</p>
+                    <Icon
+                        name='cart'
+                        color='black'
+                        className={Styles.cart}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            setOverlayState(true);
+                        }}
+                    />
+                </NavLink>
+            )}
+        </Transition>
     );
 };
 
--- src/components/_UI/MarketItemOverlay/index.js
+++ src/components/_UI/MarketItemOverlay/index.js
@@ -1,11 +1,13 @@
 // Core
 import React, { useState } from 'react';
+import { Transition } from 'react-transition-group';
 
 // Styles
 import Styles from './styles.module.scss';
 
 // Instruments
 import { Icon } from 'components';
+import { opacityTransitionConfig } from 'utils/transitionConfig';
 
 export const MarketItemOverlay = ({ className, setOverlayState, style }) => {
     // State
@@ -16,38 +18,55 @@ export const MarketItemOverlay = ({ className, setOverlayState, style }) => {
     const handleInput = ({ target: { value } }) => /^[0-9]*$/.test(value) && setAmount(value);
 
     return (
-        <section className={`${Styles.container} ${className}`} style={style}>
-            <Icon
-                name='close'
-                color='black'
-                onClick={() => setOverlayState(false)}
-                className={Styles.closeOverlay}
-            />
-            <div className={Styles.functionalButton} onClick={() => setAmount(amount + 1)}>
-                <Icon name='plus' color='black' />
-            </div>
-            {inputDisabled ? (
-                <div className={Styles.amountDisabled} onClick={() => disableInput(false)}>
-                    {amount}
-                </div>
-            ) : (
-                <input
-                    className={Styles.amountInput}
-                    onChange={handleInput}
-                    value={amount}
-                    autoFocus
-                />
+        <Transition
+            in
+            appear
+            mountOnEnter
+            unmountOnExit
+            timeout={opacityTransitionConfig().timeout}
+        >
+            {(state) => (
+                <section
+                    className={`${Styles.container} ${className}`}
+                    style={{
+                        ...style,
+                        ...opacityTransitionConfig().defaultStyles,
+                        ...opacityTransitionConfig().transitionStyles[state],
+                    }}
+                >
+                    <Icon
+                        name='close'
+                        color='black'
+                        onClick={() => setOverlayState(false)}
+                        className={Styles.closeOverlay}
+                    />
+                    <div className={Styles.functionalButton} onClick={() => setAmount(amount + 1)}>
+                        <Icon name='plus' color='black' />
+                    </div>
+                    {inputDisabled ? (
+                        <div className={Styles.amountDisabled} onClick={() => disableInput(false)}>
+                            {amount}
+                        </div>
+                    ) : (
+                        <input
+                            className={Styles.amountInput}
+                            onChange={handleInput}
+                            value={amount}
+                            autoFocus
+                        />
+                    )}
+                    <div
+                        className={Styles.functionalButton}
+                        onClick={() => setAmount(Math.max(amount - 1, 1))}
+                    >
+                        <Icon name='minus' color='black' />
+                    </div>
+                    <div className={Styles.purchaseButton}>
+                        До кошика
+                        <Icon name='cart' color='white' />
+                    </div>
+                </section>
             )}
-            <div
-                className={Styles.functionalButton}
-                onClick={() => setAmount(Math.max(amount - 1, 1))}
-            >
-                <Icon name='minus' color='black' />
-            </div>
-            <div className={Styles.purchaseButton}>
-                До кошика
-                <Icon name='cart' color='white' />
-            </div>
-        </section>
+        </Transition>
     );
 };
