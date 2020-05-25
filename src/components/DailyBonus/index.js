29851e591412a22993f246841a8dc6dd5857219e
roquefore <roquefore@gmail.com>
2020-05-25 19:35:30 +0300

    [UPD] DailyBonus -> Saving opened state

--- src/components/DailyBonus/index.js
+++ src/components/DailyBonus/index.js
@@ -1,26 +1,48 @@
 // Core
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 // Styles
 import Styles from './styles.module.scss';
 
+// Actions
+import { uiActions } from 'bus/ui/actions';
+
 const mapStateToProps = (state) => ({
-    ...state,
+    isAuthenticated: state.auth.get('isAuthenticated'),
 });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+    showLoginOverlay: uiActions.showLoginOverlay,
+};
 
-const DailyBonusComponent = ({ className }) => {
-    const [bonusChosen, setBonusChosen] = useState(false);
+const DailyBonusComponent = ({ className, isAuthenticated, showLoginOverlay }) => {
+    const [bonusOpened, setBonusOpenedState] = useState(false);
+
+    useEffect(() => {
+        const bonusOpenedDate = localStorage.getItem('opto-market-bonus-opened-date');
+
+        if (bonusOpenedDate) {
+            setBonusOpenedState(
+                isAuthenticated && bonusOpenedDate === new Date().toLocaleDateString(),
+            );
+        }
+    }, [isAuthenticated]);
+
+    const _handleBonusOpen = () => {
+        if (!isAuthenticated) return showLoginOverlay();
+
+        localStorage.setItem('opto-market-bonus-opened-date', new Date().toLocaleDateString());
+        return setBonusOpenedState(true);
+    };
 
     return (
         <section className={`${Styles.container} ${className}`}>
-            <p>Твій бонус на сьогодні:</p>
-            {bonusChosen ? (
+            <p className={Styles.title}>Твій бонус на сьогодні:</p>
+            {bonusOpened && isAuthenticated ? (
                 <div className={Styles.percent}>5%</div>
             ) : (
-                <div className={Styles.actionButton} onClick={() => setBonusChosen(true)}>
+                <div className={Styles.actionButton} onClick={_handleBonusOpen}>
                     Check
                 </div>
             )}
