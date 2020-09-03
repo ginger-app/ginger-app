b5b5f36a63dcac1baa28f968cae09c319f4d0592
roquefore-DESK <roquefore@gmail.com>
2020-09-03 21:12:16 +0200

    OP-42 [UPD] Order details page #done

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -4,17 +4,17 @@ import { connect } from 'react-redux';
 import { Transition } from 'react-transition-group';
 
 // Styles
-
-// Components
-import { OrderDetails } from 'components';
+import Styles from './styles.module.scss';
 
 // Instruments
-import { isEmpty } from 'lodash';
 import { opacityTransitionConfig } from 'utils/transitionConfig';
+import { OrderStatusLabel, Button, Navigation } from 'components';
+import eye from 'theme/assets/svg/eye.svg';
 
 // Actions
 import { marketActions } from 'bus/market/actions';
-import Styles from './styles.module.scss';
+
+const products = new Array(15).fill(eye);
 
 const mapStateToProps = (state) => ({
     orderData: state.market.get('orderData').toJS(),
@@ -25,7 +25,7 @@ const mapDispatchToProps = {
     clearOrderData: marketActions.clearOrderData,
 };
 
-const OrderComponent = ({ id, getOrderDataAsync, clearOrderData, orderData }) => {
+const OrderComponent = ({ id, getOrderDataAsync, clearOrderData }) => {
     useEffect(() => {
         getOrderDataAsync(id);
 
@@ -42,16 +42,61 @@ const OrderComponent = ({ id, getOrderDataAsync, clearOrderData, orderData }) =>
         >
             {(state) => (
                 <section
-                    className={`${Styles.bg}`}
+                    className={Styles.container}
                     style={{
                         ...opacityTransitionConfig().defaultStyles,
                         ...opacityTransitionConfig().transitionStyles[state],
                     }}
                 >
-                    <p className={Styles.title}>
-                        Замовлення <span>№{id}</span>
-                    </p>
-                    {isEmpty(orderData) ? <p>Loading....</p> : <OrderDetails id={id} />}
+                    {/* Title */}
+                    <p className={Styles.title}>Order info</p>
+
+                    {/* Order info */}
+                    <p className={Styles.date}>12.34.5678</p>
+                    <OrderStatusLabel status='Completed' className={Styles.orderStatusLabel} />
+
+                    {/* Location */}
+                    <p className={Styles.locationSubtitle}>Location:</p>
+                    <p className={Styles.location}>Forma.coffee</p>
+
+                    {/* Supplier */}
+                    <p className={Styles.supplierSubtitle}>Supplier:</p>
+                    <div className={Styles.supplier}>
+                        <span>Galychyna</span>
+                        <Button
+                            className={Styles.detailsButton}
+                            content={<img src={eye} alt='' />}
+                            onClick={() => null}
+                            filled
+                        />
+                    </div>
+
+                    {/* Order id */}
+                    <p className={Styles.orderIdSubtitle}>Order id:</p>
+                    <p className={Styles.orderId}>{id}</p>
+
+                    {/* Sum */}
+                    <p className={Styles.sumSubtitle}>Sum:</p>
+                    <p className={Styles.sum}>{100.23}</p>
+
+                    {/* Orders */}
+                    <div className={Styles.products}>
+                        {products.map((item, index) => (
+                            <div className={Styles.productItem} key={index}>
+                                <img src={item} alt='' />
+                                <p className={Styles.name}>Champion apples</p>
+                                <p className={Styles.amount}>2 kg</p>
+                                <p className={Styles.price}>87.98</p>
+                            </div>
+                        ))}
+                    </div>
+
+                    <Navigation
+                        rightButtonData={{
+                            onClick: () => null,
+                            icon: 'export',
+                        }}
+                    />
                 </section>
             )}
         </Transition>
